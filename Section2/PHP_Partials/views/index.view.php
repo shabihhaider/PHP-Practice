d8e4626dce02c673be1f17258fcbdc7c700c12b7
Muhammad Shabih Haider <123289342+shabihhaider@users.noreply.github.com>
2024-08-16 14:35:24 +0500

    Add files via upload

---
+++ Section2/PHP_Partials/views/index.view.php
@@ -0,0 +1,11 @@
+<?php require('views/partials/head.php') ?>
+<?php include('partials/nav.php') ?>
+<?php require('views/partials/banner.php') ?>
+
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <p>Hello, This is a Home Page.</p>
+  </div>
+</main>
+
+<?php require('views/partials/footer.php') ?>
