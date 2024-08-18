329dedc40006a261222ba1665cb968e0ea5b23c9
shabihhaider <2022cs723@student.uet.edu.pk>
2024-08-17 21:42:54 -0700

    PDO instance and MySQL's statement prepare, execute, and fetchAll

--- Section2/Make_a_PHP_Router/index.php
+++ Section2/Make_a_PHP_Router/index.php
@@ -2,4 +2,18 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+// connect to our MySQL database
+
+$dsn = "mysql:host=localhost;port=3306;dbname=demo;user=root;charset=utf8mb4"; // Also we can set a password here
+
+$pdo = new PDO($dsn); // PDO = PHP Data Object
+
+$statement = $pdo->prepare("SELECT * FROM posts"); // Prepare a SQL statement
+$statement->execute(); // Execute the SQL statement
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC); // Fetch all the results and remove duplication by 'FETCH_ASSOC' (Associative Array) otherwise it will return both Associative and Indexed Array
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['Title'] . "</li>";
+}
